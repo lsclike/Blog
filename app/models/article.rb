1f0a54ae0b1351cb1bdba093381093a17944b368
lsclike <lsclike2016@hotmail.com>
2017-12-21 11:39:15 +1100

    Finishing home page

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,9 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :description, presence: true, length: { minimum: 10, maximum: 140 }
-  
+  validates :title, uniqueness: true
+
+  scope :search, lambda {|query| where(["title LIKE ?", "%#{query}%"])}
+
 
 end 
